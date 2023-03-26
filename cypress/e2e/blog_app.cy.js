040817dc2142ea86a5726fed29375e5ef8d78bfb
Melina Aalto-Halme <melina.aalto-halme@tuni.fi>
2023-03-26 14:29:18 +0300

    5.21 step5 done

--- cypress/e2e/blog_app.cy.js
+++ cypress/e2e/blog_app.cy.js
@@ -20,22 +20,22 @@ describe('Blog app', function () {
     cy.contains('login').click();
   })
 
-  describe('Login',function() {
-    it('succeeds with correct credentials', function() {
+  describe('Login', function () {
+    it('succeeds with correct credentials', function () {
       cy.contains('login').click();
       cy.get('#username').type('user1');
       cy.get('#password').type('secret');
       cy.get('#loginButton').click();
-  
+
       cy.contains('Testuser logged in')
     })
 
-    it('fails with wrong credentials', function() {
+    it('fails with wrong credentials', function () {
       cy.contains('login').click();
       cy.get('#username').type('user1');
       cy.get('#password').type('wrongRassword');
       cy.get('#loginButton').click();
-  
+
       cy.contains('wrong username or password');
     })
   });
@@ -48,35 +48,52 @@ describe('Blog app', function () {
       cy.get('#loginButton').click();
     });
 
-    it('A blog can be created', function() {
+    it('A blog can be created', function () {
       cy.contains('create a new blog').click();
-      
+
       cy.get('#title').type('a blog title created by cypress');
       cy.get('#author').type('a blog author created by cypress');
       cy.get('#url').type('someurl');
-      cy.get('#createButton', {force: true}).click();
+      cy.get('#createButton', { force: true }).click();
 
       cy.contains('create').click();
       cy.contains('a blog title created by cypress');
       cy.contains('a blog author created by cypress');
     });
 
-    it('A blog can be liked', function() {
+    it('A blog can be liked', function () {
       cy.contains('create a new blog').click();
-      
+
       cy.get('#title').type('Testtitle');
       cy.get('#author').type('Testauthor');
       cy.get('#url').type('someurl');
       cy.get('#createButton').click();
 
       cy.contains('create').click();
-      cy.contains('Testtitle');
       cy.contains('Testauthor');
 
       cy.contains('view').click();
       cy.contains('likes').click();
 
       cy.contains('likes: 1');
+    });
+
+    it.only('The person who has added the blog can also remove it', function () {
+      cy.contains('create a new blog').click();
+
+      cy.get('#title').type('Testtitle');
+      cy.get('#author').type('Testauthor');
+      cy.get('#url').type('someurl');
+      cy.get('#createButton').click();
+
+      cy.contains('create').click();
+
+      cy.contains('Testtitle');
+      cy.contains('Testauthor');
+      cy.contains('view').click();
+
+      cy.contains('remove blog').click();
+      cy.contains('Testtitle').should('not.exist');
     });
   });
 });
